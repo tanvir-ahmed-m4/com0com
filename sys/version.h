73d9d61e66f6549d1a0b258db41f80f9933e12c4
vfrolov <vfrolov>
2005-09-05 15:58:34 +0000

    Initial revision

---
+++ sys/version.h
@@ -0,0 +1,35 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2005 Vyacheslav Frolov
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifndef _C0C_VERSION_H_
+#define _C0C_VERSION_H_
+
+#define C0C_V1 1
+#define C0C_V2 3
+#define C0C_V3 0
+#define C0C_V4 1
+
+#define MK_VERSION_STR1(V1, V2, V3, V4) #V1 "." #V2 "." #V3 "." #V4
+#define MK_VERSION_STR(V1, V2, V3, V4) MK_VERSION_STR1(V1, V2, V3, V4)
+
+#define C0C_VERSION_STR MK_VERSION_STR(C0C_V1, C0C_V2, C0C_V3, C0C_V4)
+
+#endif /* _C0C_VERSION_H_ */
