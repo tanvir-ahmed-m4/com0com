dfc2d2093cd6dcbf132bc1e5712957723f197e88
vfrolov <vfrolov>
2006-10-13 10:06:13 +0000

    Initial revision

---
+++ include/com0com.h
@@ -0,0 +1,42 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2006 Vyacheslav Frolov
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *
+ * $Log$
+ * Revision 1.1  2006/10/13 10:06:13  vfrolov
+ * Initial revision
+ *
+ *
+ */
+
+#ifndef _INCLUDE_C0C_COM0COM_H_
+#define _INCLUDE_C0C_COM0COM_H_
+
+#define C0C_BUS_DEVICE_ID           TEXT_PREF"root\\com0com"
+#define C0C_PORT_DEVICE_ID          TEXT_PREF"com0com\\port"
+#define C0C_SERVICE                 TEXT_PREF"com0com"
+#define C0C_PREF_NT_DEVICE_NAME     TEXT_PREF"\\Device\\"
+
+#define C0C_PREF_BUS_NAME           TEXT_PREF"CNCBUS"
+#define C0C_PREF_PORT_NAME_A        TEXT_PREF"CNCA"
+#define C0C_PREF_PORT_NAME_B        TEXT_PREF"CNCB"
+#define C0C_PREF_DEVICE_NAME_A      C0C_PREF_NT_DEVICE_NAME C0C_SERVICE TEXT_PREF"1"
+#define C0C_PREF_DEVICE_NAME_B      C0C_PREF_NT_DEVICE_NAME C0C_SERVICE TEXT_PREF"2"
+
+#endif /* _INCLUDE_C0C_COM0COM_H_ */
