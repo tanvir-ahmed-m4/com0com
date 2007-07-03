b4a10762895c86cd9404b01a5737acdc3bbdaddf
vfrolov <vfrolov>
2007-07-03 14:25:12 +0000

    Implemented pinout customization

--- include/com0com.h
+++ include/com0com.h
@@ -1,7 +1,7 @@
 /*
  * $Id$
  *
- * Copyright (c) 2006 Vyacheslav Frolov
+ * Copyright (c) 2006-2007 Vyacheslav Frolov
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,6 +19,9 @@
  *
  *
  * $Log$
+ * Revision 1.4  2007/07/03 14:25:12  vfrolov
+ * Implemented pinout customization
+ *
  * Revision 1.3  2006/11/03 13:06:33  vfrolov
  * Moved C0C_PORT_NAME_LEN from sys/com0com.h to include/com0com.h
  *
@@ -46,5 +49,14 @@
 #define C0C_PREF_DEVICE_NAME_B      C0C_PREF_NT_DEVICE_NAME C0C_SERVICE TEXT_PREF"2"
 
 #define C0C_REGSTR_VAL_PORT_NUM     TEXT_PREF"PortNum"
+
+#define C0C_PIN_RRTS                0x00000001
+#define C0C_PIN_RDTR                0x00000002
+#define C0C_PIN_ROUT1               0x00000004
+#define C0C_PIN_LRTS                0x00000100
+#define C0C_PIN_LDTR                0x00000200
+#define C0C_PIN_LOUT1               0x00000400
+#define C0C_PIN_ON                  0x10000000
+#define C0C_PIN_NEGATIVE            0x80000000
 
 #endif /* _INCLUDE_C0C_COM0COM_H_ */
