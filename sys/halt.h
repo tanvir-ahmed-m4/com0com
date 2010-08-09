22db71ac719488115f8113ff126505edd171e96b
vfrolov <vfrolov>
2010-08-09 12:50:58 +0000

    Added try DbgBreakPoint() before KeBugCheckEx()

--- sys/halt.h
+++ sys/halt.h
@@ -19,6 +19,9 @@
  *
  *
  * $Log$
+ * Revision 1.3  2010/08/09 12:50:58  vfrolov
+ * Added try DbgBreakPoint() before KeBugCheckEx()
+ *
  * Revision 1.2  2010/08/04 10:38:55  vfrolov
  * Minimized PREfast noise
  *
@@ -31,19 +34,21 @@
 #define _HALT_H_
 
 #if defined(_PREFAST_)
-
-#define HALT_UNLESS3(exp, p1, p2, p3) __assume(exp)
-
+  #define HALT_UNLESS3(exp, p1, p2, p3) __assume(exp)
 #elif DBG
-
-#define HALT_UNLESS3(exp, p1, p2, p3) \
-  if (!(exp)) \
-    KeBugCheckEx(0xDEADC0CD, (FILE_ID << 16) + __LINE__, p1, p2, p3)
-
+  #define HALT_UNLESS3(exp, p1, p2, p3) \
+    if (!(exp)) { \
+      try { \
+        DbgPrint("%s (%d):\n", __FILE__, __LINE__); \
+        DbgPrint("  HALT_UNLESS3(\"%s\",\n", #exp); \
+        DbgPrint("               0x%08X, 0x%08X, 0x%08X);\n", (ULONG)(p1), (ULONG)(p2), (ULONG)(p3)); \
+        DbgBreakPoint(); \
+      } except (EXCEPTION_EXECUTE_HANDLER) { \
+        KeBugCheckEx(0xDEADC0CD, (FILE_ID << 16) + __LINE__, (ULONG_PTR)(p1), (ULONG_PTR)(p2), (ULONG_PTR)(p3)); \
+      } \
+    }
 #else
-
-#define HALT_UNLESS3(exp, p1, p2, p3)
-
+  #define HALT_UNLESS3(exp, p1, p2, p3)
 #endif
 
 #define HALT_UNLESS2(exp, p1, p2) HALT_UNLESS3(exp, p1, p2, 0)
